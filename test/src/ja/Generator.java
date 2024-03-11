c8f6c2b6c27a0d37c3ed468a2353e5ce0bf4e23b
whitok355 <3139139@mail.ru>
2024-03-11 14:11:00 +0300

    hw4

--- test/src/ja/Generator.java
+++ test/src/ja/Generator.java
@@ -1,37 +1,19 @@
 package ja;
 
-
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Generator {
-    private static Random rand = new Random();
-
-    private static double getRandomDouble(int max, int min){
-        MathContext context = new MathContext(2, RoundingMode.HALF_UP);
-        BigDecimal result = new BigDecimal(rand.nextDouble(max,min), context);
-        return result.doubleValue();
-    }
-    private static int getRandomInt(int max, int min){
-        return rand.nextInt(max, min);
-    }
-    public static People generateWorkers(int id){
-        int rand = getRandomInt(0, 2);
-        if(rand == 1){
-            return new Freelancer(
-                    id,
-                    getRandomDouble(0, 23),
-                    getRandomDouble(100, 500) //ставка за час
-            );
-        } else{
-            return new Worker(
-                    id,
-                    getRandomDouble(0, 23 ),
-                    getRandomDouble(500, 5000) //ставка за день
-            );
+    static String generateNumberAccount(){
+        Random rn = new Random();
+        String numb = "";
+        for(int i = 0; i < 4; i++){
+            for(int p = 0 ; p < 4; p++){
+                numb += rn.nextInt(0, 9);
+            }
+            if(i < 3){
+                numb += "-";
+            }
         }
+        return numb;
     }
 }
