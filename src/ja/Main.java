ce951e3e69ce8a7b0008bc6a2bf75f30b323ad05
whitok355 <3139139@mail.ru>
2024-03-11 14:11:00 +0300

    hw4 v1.5

---
+++ src/ja/Main.java
@@ -0,0 +1,11 @@
+package ja;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Program pr = new Program();
+
+        pr.createAcc();
+    }
+}
---
+++ src/ja/Msg.java
@@ -0,0 +1,7 @@
+package ja;
+
+public class Msg {
+    public static void printMsg(String msg){
+        System.out.println(msg);
+    }
+}
